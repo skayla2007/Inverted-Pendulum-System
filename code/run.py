de5872395a23f9e2eadb0f4d0e4c5e1bd2326691
skayla2007 <fty13073109966@Gmail.com>
2026-03-21 23:23:41 +0800

    run施加干扰

--- code/run.py
+++ code/run.py
@@ -1,5 +1,7 @@
 # run.py
 import time
+import numpy as np
+import pybullet as p
 from stable_baselines3 import PPO
 from inverted_pendulum_env import InvertedPendulum3D
 
@@ -8,10 +10,25 @@ def run():
     model = PPO.load("kinematic_balance_model")
     obs, _ = env.reset()
     while True:
-        action, _ = model.predict(obs)
-        obs, _, terminated, _, _ = env.step(action)
+        keys = p.getKeyboardEvents()
+        force_x, force_y = 0, 0
+        push_magnitude = 0.01
+
+        if p.B3G_LEFT_ARROW in keys and keys[p.B3G_LEFT_ARROW] & p.KEY_IS_DOWN: force_x = -push_magnitude
+        if p.B3G_RIGHT_ARROW in keys and keys[p.B3G_RIGHT_ARROW] & p.KEY_IS_DOWN: force_x = push_magnitude
+        if p.B3G_UP_ARROW in keys and keys[p.B3G_UP_ARROW] & p.KEY_IS_DOWN: force_y = push_magnitude
+        if p.B3G_DOWN_ARROW in keys and keys[p.B3G_DOWN_ARROW] & p.KEY_IS_DOWN: force_y = -push_magnitude
+
+        if force_x != 0 or force_y != 0:
+            p.applyExternalForce(env.pole_id, -1,
+                                 forceObj=[force_x, force_y, 0],
+                                 posObj=[0, 0, 0],
+                                 flags=p.WORLD_FRAME)
+
+        action, _ = model.predict(obs, deterministic=True)
+        obs, _, terminated, truncated, _ = env.step(action)
         time.sleep(1/120.0)
-        if terminated: obs, _ = env.reset()
+        if terminated or truncated: obs, _ = env.reset()
 
 if __name__ == "__main__":
     run()
